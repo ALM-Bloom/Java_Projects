e19de2a3ca0506a742bbcd2de2fed7faedaab643
AlejandroML <72447092+ALM-Bloom@users.noreply.github.com>
2025-04-29 23:30:08 +0100

    Comentado filosofía de la aplicación

--- BinaryTree/src/App.java
+++ BinaryTree/src/App.java
@@ -1,3 +1,14 @@
+/*
+ * Fecha de Comienzo: 29/04/2025
+ * Fecha de Finalización: 
+ * Autor: Alejandro M.L, durante tardeos.
+ * Este programa tienen como función la didáctica en cuanto a la implementación de una estructura de árbol binario en el lenguaje Java.
+ * El tipo de datos de los nodos será unícamente de valores enteros.
+ * Se ha establecido una ramificación de 2, esto es por definición, la implementación de un árbol binario.
+ * Se pretende implementar por tanto, las clásicas funciones de inserción, búsqueda y eliminación. Todas de manera recursiva.
+ * 
+ * Dada la naturaleza de este proyecto (asentamiento del lenguaje y entretenimiento), se pretende desarrollar todo el código sin el uso de IA generativa (inclusive Copilot).
+ */
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
