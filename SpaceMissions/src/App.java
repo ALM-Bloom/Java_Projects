33d1015663b10c9f8a02a33b5e10f5f10f94cb37
Ale M.L <alejandro.melian.lemes.31@ull.edu.es>
2025-08-11 03:14:03 +0100

    Creado archivo 'main' del programa

---
+++ SpaceMissions/src/App.java
@@ -0,0 +1,19 @@
+/*
+ * Fecha de Comienzo: 11/08/2025
+ * Fecha de Finalización:
+ * Autor: Alejandro M.L, durante tardeos.
+ * Descripción: Este proyecto tiene como objetivo la creación de Aplicación por 
+ * terminal en Java que simule el control de una agencia espacial para gestionar misiones
+ * interplanetarias.
+ * El principal recurso para la asignación y desarrollo de misiones son las catalogadas como "naves".
+ *
+ * Dada la naturaleza de este proyecto (asentamiento del lenguaje y entretenimiento), se pretende desarrollar todo el código sin el uso de IA generativa (inclusive Copilot).
+ * Aunque... la idea del proyecto dado que tiene función didáctica, ha sido generada por IA.
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
