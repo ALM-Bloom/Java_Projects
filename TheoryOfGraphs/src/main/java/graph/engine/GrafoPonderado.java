1ec0b6171778a17c61282b570397ad4012821978
AlejandroML <72447092+ALM-Bloom@users.noreply.github.com>
2026-02-11 18:52:30 +0000

    Definida nueva interfaz para grafos ponderados
    Tras reflexionar sobre la modularidad del proyecto, he decidido separar por completo las plantillas que deberán definir los dos tipos distintos de grafos: ponderados y no ponderados.

---
+++ TheoryOfGraphs/src/main/java/graph/engine/GrafoPonderado.java
@@ -0,0 +1,40 @@
+package graph.engine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public interface GrafoPonderado {
+
+    public HashSet<Integer> getVertices();
+
+    public HashMap<Integer, HashSet<Arista>> getAristas();
+
+    public void setVertices(HashSet<Integer> vertices);
+
+    public void setAristas(HashMap<Integer, HashSet<Arista>> aristas);
+
+    public int totalAristas();
+
+    public boolean[][] convertMatrizAdyacencia();
+
+    public boolean[][] convertirMatrizIncidencia();
+
+    public ArrayList<Integer> listaGrados();
+
+    public HashMap<Integer, HashSet<Integer>> listaAdyacencia();
+
+    public void mostrarMatriz(boolean[][] matrix);
+
+    public void MostrarMatrizCostos(double[][] matrix);
+
+    public void mostrarLista(HashMap<Integer, HashSet<Integer>> adyacencias);
+
+    public void mostrarGrados(ArrayList<Integer> grados);
+
+    public String BFS(int fuente);
+
+    public String DFS(int fuente);
+
+    public double[][] convertirMatrizCostos();
+}
